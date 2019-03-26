4c40439a948f5f4d86c56fbae8301dbe1eb42e89
w.h.sinn <w.h.sinn@gmail.com>
2019-03-26 19:41:52 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,6 +15,16 @@ def contain_a(array)
   container
 end
 
+def first_wa(array)
+  first_wa = nil
+  array.each do |element|
+    if element.match(/wa/)
+      first_wa = element
+      break
+    end
+  end
+  first_wa
+end
 
 def remove_non_strings(array)
   container = []
