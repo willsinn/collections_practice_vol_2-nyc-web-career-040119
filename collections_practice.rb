30e1dc52b1bbc47167bcb52b26c66d63828091d0
w.h.sinn <w.h.sinn@gmail.com>
2019-03-26 19:42:16 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -26,6 +26,19 @@ def first_wa(array)
   first_wa
 end
 
+
+def count_elements(array)
+  array.each do |original_hash|
+    original_hash[:count] = 0
+    name = original_hash[:name]
+    array.each do |hash|
+      if hash[:name] == name
+        original_hash[:count] += 1
+      end
+    end
+  end.uniq
+end
+
 def remove_non_strings(array)
   container = []
   array.each do |element|
